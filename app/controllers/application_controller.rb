4215815beb83ecedc9925783476761d392c9ad29
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-11-28 20:19:59 +0300

    Completed assigment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  # private
+  
+  # def current_user_session
+  #   @_current_user ||= session[:current_user_id] &&
+  #     User.find_by(id: session[:current_user_id])
+  # end
+
 end
